910b44831d6eb78553b78a845e01896b723b0ae9
Shaji Khan <git@shaji.in>
2023-04-21 13:17:15 +0530

    ndk build

---
+++ Rumor/jni/xresources.h
@@ -0,0 +1,4 @@
+EXTLD(footsw_png)
+EXTLD(fx_knob_png)
+EXTLD(LED_Red_png)
+EXTLD(monster_png)
---
+++ Rumor/obj/local/arm64-v8a/objs/Rumor/Rumor.o.d
@@ -0,0 +1,20 @@
+./obj/local/arm64-v8a/objs/Rumor/Rumor.o: jni/Rumor.cpp jni/resampler.cc \
+  jni/zita-resampler/resampler.h jni/zita-resampler/resampler-table.h \
+  jni/resampler-table.cc jni/gx_resampler.cc jni/gx_resampler.h \
+  jni/rumor.cc jni/rumor_table.h
+
+jni/resampler.cc:
+
+jni/zita-resampler/resampler.h:
+
+jni/zita-resampler/resampler-table.h:
+
+jni/resampler-table.cc:
+
+jni/gx_resampler.cc:
+
+jni/gx_resampler.h:
+
+jni/rumor.cc:
+
+jni/rumor_table.h:
